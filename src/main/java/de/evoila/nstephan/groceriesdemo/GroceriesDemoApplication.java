4a762e43194c8720711dcaf3b0401e196a46da2b
Nikita Stephan <nstephan@evoila.de>
2022-09-12 21:15:03 +0200

    initial commit

---
+++ src/main/java/de/evoila/nstephan/groceriesdemo/GroceriesDemoApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.nstephan.groceriesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceriesDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/evoila/nstephan/groceriesdemo/GroceriesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.nstephan.groceriesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
