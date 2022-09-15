d1e26b30a24b62809c5f992da26011de505edb0e
Nikita Stephan <nstephan@evoila.de>
2022-09-15 16:29:04 +0200

    change repository superinterface

--- src/main/java/de/evoila/nstephan/groceriesdemo/repository/GroceryItemRepository.java
+++ src/main/java/de/evoila/nstephan/groceriesdemo/repository/GroceryItemRepository.java
@@ -1,9 +1,9 @@
 package de.evoila.nstephan.groceriesdemo.repository;
 
 import de.evoila.nstephan.groceriesdemo.model.GroceryItem;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "groceries", path = "groceries")
-public interface GroceryItemRepository extends PagingAndSortingRepository<GroceryItem, Long> {
+public interface GroceryItemRepository extends JpaRepository<GroceryItem, Long> {
 }
