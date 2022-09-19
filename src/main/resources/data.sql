81b05b1fff2bb5d90f76834faee6d373dcb664d1
Nikita Stephan <nstephan@evoila.de>
2022-09-19 12:20:55 +0200

    prepopulate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Kokoswasser', '2 Pck.');
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Hackfleisch', '1 kg');
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Milch', '1 Flasche');
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Avocado', '1 Stk.');
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Eier', '12');
+insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
+values (next value for hibernate_sequence, 'Wasser', '12 l');
