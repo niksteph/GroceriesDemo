5f906e3934c8b15c0601c68aabac79496ff9c7ab
Nikita Stephan <nstephan@evoila.de>
2022-09-21 12:47:26 +0200

    implement mapping for store

--- src/main/java/de/evoila/nstephan/groceriesdemo/mapping/GroceryItemMapper.java
+++ src/main/java/de/evoila/nstephan/groceriesdemo/mapping/GroceryItemMapper.java
@@ -2,10 +2,25 @@ package de.evoila.nstephan.groceriesdemo.mapping;
 
 import de.evoila.nstephan.groceriesdemo.dto.GroceryItemDTO;
 import de.evoila.nstephan.groceriesdemo.model.GroceryItem;
+import de.evoila.nstephan.groceriesdemo.model.Store;
+import lombok.Setter;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Mapper(componentModel = "spring")
-public interface GroceryItemMapper {
+public abstract class GroceryItemMapper {
 
-    GroceryItem dtoToEntity(GroceryItemDTO dto);
+    @PersistenceContext
+    @Setter
+    private EntityManager em;
+
+    @Mapping(source = "storeId", target = "store")
+    public abstract GroceryItem dtoToEntity(GroceryItemDTO dto);
+
+    public Store storeFromId(Long storeId) {
+        return em.find(Store.class, storeId);
+    }
 }
