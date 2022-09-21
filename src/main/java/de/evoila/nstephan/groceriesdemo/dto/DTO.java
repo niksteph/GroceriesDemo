388adfdd0695c9b266d7089d7cb19c5324b3e5fd
Nikita Stephan <nstephan@evoila.de>
2022-09-21 12:31:01 +0200

    remove unnecessary dto superclass

--- src/main/java/de/evoila/nstephan/groceriesdemo/dto/DTO.java
+++
@@ -1,8 +0,0 @@
-package de.evoila.nstephan.groceriesdemo.dto;
-
-public abstract class DTO {
-
-    abstract Object entity();
-
-    abstract Object entity(Long id);
-}
--- src/main/java/de/evoila/nstephan/groceriesdemo/dto/GroceryItemDTO.java
+++ src/main/java/de/evoila/nstephan/groceriesdemo/dto/GroceryItemDTO.java
@@ -1,10 +1,9 @@
 package de.evoila.nstephan.groceriesdemo.dto;
 
-import de.evoila.nstephan.groceriesdemo.model.GroceryItem;
 import lombok.Data;
 
 @Data
-public class GroceryItemDTO extends DTO {
+public class GroceryItemDTO {
 
     private Long id;
 
@@ -15,14 +14,4 @@ public class GroceryItemDTO extends DTO {
     private String comment;
 
     private Long storeId;
-
-    @Override
-    public GroceryItem entity() {
-        return null;
-    }
-
-    @Override
-    public GroceryItem entity(Long id) {
-        return null;
-    }
 }
