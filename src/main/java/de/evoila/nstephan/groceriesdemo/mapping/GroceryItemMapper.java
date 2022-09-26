1eb56d2bbb2f32dab940084c22eec31b5ed43a7b
Nikita Stephan <nstephan@evoila.de>
2022-09-26 17:11:49 +0200

    create mapping for json merge patch

--- src/main/java/de/evoila/nstephan/groceriesdemo/mapping/GroceryItemMapper.java
+++ src/main/java/de/evoila/nstephan/groceriesdemo/mapping/GroceryItemMapper.java
@@ -1,14 +1,14 @@
 package de.evoila.nstephan.groceriesdemo.mapping;
 
 import de.evoila.nstephan.groceriesdemo.dto.GroceryItemDTO;
+import de.evoila.nstephan.groceriesdemo.dto.GroceryItemPatchDTO;
 import de.evoila.nstephan.groceriesdemo.model.GroceryItem;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 /**
- * Mapper for mapping between {@link GroceryItem} and {@link GroceryItemDTO}.
+ * Mapper for mapping between {@link GroceryItem} and it's DTOs.
  */
-@Mapper(componentModel = "spring", uses = EntityMapper.class)
+@Mapper(componentModel = "spring", uses = { EntityMapper.class, JsonNullableMapper.class })
 public interface GroceryItemMapper {
 
     /**
@@ -19,4 +19,16 @@ public interface GroceryItemMapper {
      */
     @Mapping(source = "storeId", target = "store")
     GroceryItem dtoToEntity(GroceryItemDTO dto);
+
+    /**
+     * Maps {@link GroceryItemPatchDTO} to {@link GroceryItem}.
+     * Used for JSON Merge Patch
+     *
+     * @param dto
+     * @return the entity
+     */
+    @Mapping(source = "storeId", target = "store")
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchEntity(@MappingTarget GroceryItem item, GroceryItemPatchDTO dto);
 }
