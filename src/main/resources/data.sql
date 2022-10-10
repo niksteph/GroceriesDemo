edcad2336e8745c57b9a2052057d52a21c238298
Nikita Stephan <nstephan@evoila.de>
2022-10-10 13:25:13 +0200

    replace h2 with postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Kokoswasser', '2 Pck.');
+values (nextval('hibernate_sequence'), 'Kokoswasser', '2 Pck.');
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Hackfleisch', '1 kg');
+values (nextval('hibernate_sequence'), 'Hackfleisch', '1 kg');
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Milch', '1 Flasche');
+values (nextval('hibernate_sequence'), 'Milch', '1 Flasche');
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Avocado', '1 Stk.');
+values (nextval('hibernate_sequence'), 'Avocado', '1 Stk.');
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Eier', '12');
+values (nextval('hibernate_sequence'), 'Eier', '12');
 insert into GROCERY_ITEM (ID, DESCRIPTION, AMOUNT)
-values (next value for hibernate_sequence, 'Wasser', '12 l');
+values (nextval('hibernate_sequence'), 'Wasser', '12 l');
 
 insert into STORE (ID, NAME)
-values (next value for hibernate_sequence, 'Rewe');
+values (nextval('hibernate_sequence'), 'Rewe');
